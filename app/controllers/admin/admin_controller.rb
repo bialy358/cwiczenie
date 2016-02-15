0c129410c19b7f003116fe523faf3d935201e748
Bialy <bialy358@gmail.com>
2016-02-15 11:13:54 +0100

    refactor admin layout

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,15 +1,12 @@
 class Admin::AdminController < ApplicationController
-
   before_action :require_admin
   layout 'admin'
-
-
 
   private
 
   def require_admin
     return if current_user && current_user.admin
-    redirect_to root_path, alert: "Entrance permited. Admin require"
+    redirect_to root_path, alert: t('admin.failure')
   end
 
 end
