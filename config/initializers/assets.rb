f7acd65c254befb5aaf8636323dc58e8ad7f24e3
Bialy <bialy358@gmail.com>
2016-02-17 11:58:30 +0100

    fixe typo

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( control_panel/application.js )
-Rails.application.config.assets.precompile += %w( control_panel/applicationl.scss )
+Rails.application.config.assets.precompile += %w( control_panel/application.scss )
