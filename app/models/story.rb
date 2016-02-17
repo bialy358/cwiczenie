4a376199037593e399dc74cabd4423cdb4255d75
Bialy <bialy358@gmail.com>
2016-02-17 22:55:14 +0100

    Refactor of models

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,9 +2,9 @@ class Story < ActiveRecord::Base
   belongs_to :board
 
   ESTIMATES = [ 1, 2, 3, 5, 8, 13, 20 ]
-  enum status: [:status1, :status2]
+  enum status: { draft: 10, active: 20, done: 30 }
 
-  validates :name, presence: true
+  validates :title, presence: true
   validates :estimate , inclusion: { in: ESTIMATES }, presence: true
-
+  validates :description, length: {maximum: 500}
 end
