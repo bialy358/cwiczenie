ec24fbaeac53476c8f2bc2c9c44df31317ab991d
Bialy <bialy358@gmail.com>
2016-03-04 16:54:33 +0100

    refactor of specs to test pass

--- spec/controllers/control_panel/boards_controller_spec.rb
+++ spec/controllers/control_panel/boards_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ControlPanel::BoardsController do
+  let(:owner) { create :user}
   let(:user) { create :user }
-  let(:board) {create :board, owner_id: user.id}
-
+  let(:board) { create :board, owner_id: owner.id }
+  let(:owner_member) { create :member, user_id: owner.id, board_id: board.id}
+  let(:member) { create :member, user_id: user.id, board_id: board.id}
   context "User signed in" do
-    before { sign_in(user) }
+    before  { sign_in(user) }
 
     describe " GET #index" do
       it "renders boards#index" do
@@ -61,82 +63,152 @@ RSpec.describe ControlPanel::BoardsController do
       end #context 'failure'
     end
 
-    describe "GET #show" do
-      it "renders boards#show" do
-        get :show, id: board.id
-        expect(response).to render_template :show
+    context "is member of board" do
+      before { member }
+
+      describe "GET #show" do
+        it "renders boards#show" do
+          get :show, id: board.id
+          expect(response).to render_template :show
+        end
       end
-    end
 
-    describe "GET #edit" do
-      it "renders boards#edit" do
-        get :edit, id: board.id
-        expect(response).to render_template :edit
+      describe "GET #edit" do
+        it "redirects to boards index path" do
+          get :edit, id: board.id
+          expect(response).to redirect_to control_panel_board_path(board.id)
+        end
+
+        it "flashes alert" do
+          expect(flash[:alert]).presence
+        end
       end
-    end
 
-    describe "PUT #update" do
-      let!(:board) {create :board, owner_id: user.id}
-      let!(:params) do
-        { id: board.id, board: { name: 'cokolwiek'} }
+      describe "PUT #update" do
+        let!(:board) {create :board, owner_id: owner.id}
+        let!(:params) do
+          { id: board.id, board: { name: 'cokolwiek'} }
+        end
+        let(:request) { put :update, params }
+
+        context "failure" do
+          it "no changes in name of board" do
+            expect { request }.to_not change{ board.reload.name }
+          end
+
+          it "redirects to boards index path" do
+            request
+            expect(response).to redirect_to control_panel_board_path(board.id)
+          end
+
+          it "flashes message alert" do
+            request
+            expect(flash[:alert]).presence
+          end
+        end #context 'failure'
       end
-      let(:request) { put :update, params }
 
-      context "success" do
-        it "changes name of boards" do
-          expect { request }.to change{ board.reload.name }.to('cokolwiek')
-        end
+      describe "DELETE #destroy" do
+        let!(:board) { create :board, owner_id: owner.id }
+        context "failure" do
+          let(:request) { delete :destroy, id: board.id }
 
-        it "redirects properly" do
-          request
-          expect(response).to redirect_to control_panel_root_path
-        end
+          it "change count of Board by -1" do
+            expect{ request }.to_not change{ Board.count }
+          end
 
-        it "gives 302 http code" do
-          request
-          expect(response).to have_http_status(302)
-        end
+          it "redirects to root" do
+            request
+            expect(response).to redirect_to control_panel_board_path(board.id)
+          end
 
-        it "flashes message" do
-          request
-          expect(flash[:notice]).to eq I18n.t('shared.updated')
-        end
-      end #context 'success'
+          it "flashes message" do
+            request
+            expect(flash[:alert]).presence
+          end
+        end #context failure
+      end
+    end #context is member of board
 
-      context "failure" do
-        before do
-          allow_any_instance_of(Board).to receive(:update) { false }
-          request
-        end
+    context "is owner of board" do
+      before do
+        sign_in(owner)
+        owner_member
+      end
 
-        it "renders boards#edit" do
+      describe "GET #edit" do
+        it "renders properly" do
+          get :edit, id: board.id
           expect(response).to render_template :edit
         end
+      end
 
-        it "doesn't change Board name" do
-          expect { request }.to_not change{ board.reload.name }
+      describe "PUT #update" do
+        let!(:board) {create :board, owner_id: owner.id}
+        let!(:params) do
+          { id: board.id, board: { name: 'cokolwiek'} }
         end
-      end #context 'failure'
-    end
+        let(:request) { put :update, params }
 
-    describe "DELETE #destroy" do
-      let!(:board) { create :board }
-      let(:request) { delete :destroy, id: board.id }
+        context "success" do
+          it "changes name of boards" do
+            expect { request }.to change{ board.reload.name }.to('cokolwiek')
+          end
 
-      it "change count of Board by -1" do
-        expect{ request }.to change{ Board.count }.by(-1)
+          it "redirects properly" do
+            request
+            expect(response).to redirect_to control_panel_root_path
+          end
+
+          it "gives 302 http code" do
+            request
+            expect(response).to have_http_status(302)
+          end
+
+          it "flashes message" do
+            request
+            expect(flash[:notice]).to eq I18n.t('shared.updated')
+          end
+        end #context 'success'
+
+        context "failure" do
+          before do
+            allow_any_instance_of(Board).to receive(:update) { false }
+            request
+          end
+
+          it "renders boards#edit" do
+            expect(response).to render_template :edit
+          end
+
+          it "doesn't change Board name" do
+            expect { request }.to_not change{ board.reload.name }
+          end
+        end #context 'failure'
       end
 
-      it "redirects to root" do
-        request
-        expect(response).to redirect_to control_panel_root_path
-      end
+      describe "DELETE #destroy" do
+        let!(:board) { create :board, owner_id: owner.id }
 
-      it "flashes message" do
-        request
-        expect(flash[:notice]).to eq I18n.t('shared.destroyed')
+        context "success" do
+        let(:request) { delete :destroy, id: board.id }
+
+          it "change count of Board by -1" do
+            expect{ request }.to change{ Board.count }.by(-1)
+          end
+
+          it "redirects to root" do
+            request
+            expect(response).to redirect_to control_panel_root_path
+          end
+
+          it "flashes message" do
+            request
+            expect(flash[:notice]).to eq I18n.t('shared.destroyed')
+          end
+        end #context 'success'
       end
-    end
+    end #context 'is owner of board'
   end # context 'User signed in'
 
   context "User not signed in" do
@@ -148,7 +220,7 @@ RSpec.describe ControlPanel::BoardsController do
 
       it "flashes user auth warning message" do
         get :index
-        expect(flash[:alert]).to eq I18n.t('user.auth.failure')
+        expect(flash[:alert]).presence
       end
     end
 
@@ -160,7 +232,7 @@ RSpec.describe ControlPanel::BoardsController do
 
       it "flashes user auth warning message" do
         get :new
-        expect(flash[:alert]).to eq I18n.t('user.auth.failure')
+        expect(flash[:alert]).presence
       end
     end
 
@@ -173,7 +245,7 @@ RSpec.describe ControlPanel::BoardsController do
 
       it "flashes user auth warning message" do
         request
-        expect(flash[:alert]).to eq I18n.t('user.auth.failure')
+        expect(flash[:alert]).presence
       end
 
       it "redirects to root" do
@@ -190,7 +262,7 @@ RSpec.describe ControlPanel::BoardsController do
 
       it "flashes user auth warning message" do
         get :show, id: board.id
-        expect(flash[:alert]).to eq I18n.t('user.auth.failure')
+        expect(flash[:alert]).presence
       end
     end
 
@@ -202,7 +274,7 @@ RSpec.describe ControlPanel::BoardsController do
 
       it "flashes user auth warning message" do
         get :edit, id: board.id
-        expect(flash[:alert]).to eq I18n.t('user.auth.failure')
+        expect(flash[:alert]).presence
       end
     end
 
@@ -219,7 +291,7 @@ RSpec.describe ControlPanel::BoardsController do
 
       it "flashes user auth warning message" do
         request
-        expect(flash[:alert]).to eq I18n.t('user.auth.failure')
+        expect(flash[:alert]).presence
       end
 
       it "redirects to root" do
@@ -243,7 +315,7 @@ RSpec.describe ControlPanel::BoardsController do
 
       it "flashes user auth warning message" do
         request
-        expect(flash[:alert]).to eq I18n.t('user.auth.failure')
+        expect(flash[:alert]).presence
       end
     end
   end #context 'User not signed in'
