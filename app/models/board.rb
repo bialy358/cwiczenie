e75714655f3c07117af236c3687d747c960493c4
Bialy <bialy358@gmail.com>
2016-03-04 16:52:16 +0100

    remove empty lines

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,12 +1,9 @@
 class Board < ActiveRecord::Base
-
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   has_many :users, through: :members
   has_many :members
-
   has_many :stories, dependent: :destroy
 
   validates :name, presence: true
   validates :name, uniqueness: {scope: :owner_id}
-
 end
