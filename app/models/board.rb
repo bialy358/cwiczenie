c170a0c474d39828265043b73cd4bf5a06f97e02
Bialy <bialy358@gmail.com>
2016-03-11 23:50:13 +0100

    Add destroy dependencies

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,7 @@
 class Board < ActiveRecord::Base
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   has_many :users, through: :members
-  has_many :members
+  has_many :members, dependent: :destroy
   has_many :stories, dependent: :destroy
 
   validates :name, presence: true
